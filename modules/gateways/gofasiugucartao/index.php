be9ddebf2fa3aea3988c9f0c1c9268e8dfab5634
Mauricio Gofas <mauricio@gofas.net>
2024-03-26 23:43:23 -0300

    v1.0.0 beta 2

--- modules/gateways/gofasiugucartao/index.php
+++ modules/gateways/gofasiugucartao/index.php
@@ -7,7 +7,15 @@
  * @support		https://gofas.net/?p=12349
  * @version		1.0.0
  */
-require_once __DIR__.'/includes/hooks.php';
-require_once __DIR__.'/includes/config.php';
-require_once __DIR__.'/includes/3dsecure.php';
-require_once __DIR__.'/includes/capture.php';
+if((int)substr(preg_replace('/[^\da-z]/i','',phpversion()),0,2)>=(int)81){
+	require_once __DIR__.'/includes/hooks.php';
+    require_once __DIR__.'/includes/config.php';
+    require_once __DIR__.'/includes/3dsecure.php';
+    require_once __DIR__.'/includes/capture.php';
+}
+if((int)substr(preg_replace('/[^\da-z]/i','',phpversion()),0,2)<=(int)74){
+    require_once __DIR__.'/includesd/hooks.php';
+    require_once __DIR__.'/includesd/config.php';
+    require_once __DIR__.'/includesd/3dsecure.php';
+    require_once __DIR__.'/includesd/capture.php';
+}
