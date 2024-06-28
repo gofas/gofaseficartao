bbda01a8698f5a59135be6e9eb7a675a07eba9fa
Mauricio Gofas <mauricio@gofas.net>
2024-06-28 13:35:49 -0300

    git sync

---
+++ modules/gateways/gofaseficartao/assets/js/ggnc.js
@@ -0,0 +1,25 @@
+/**
+ * Módulo Gofas Gerencinet Cartão para WHMCS
+ * @author		Gofas Software
+ * @see			https://gofas.net/?p=8423
+ * @copyright	2016 - 2020 https://gofas.net
+ * @license		https://gofas.net?p=9340
+ * @support		https://gofas.net/?p=8343
+ * @version		3.1.2
+ */
+/*
+ *	Cartões aceitos:
+ *
+ *	visa // Bandeira Visa
+ *	mastercard // Bandeira MasterCard
+ *	jcb // Bandeira JCB
+ *	diners // Bandeira Dinners
+ *	amex // Bandeira AmericanExpress
+ *	discover // Bandeira Discover
+ *	elo // Bandeira Elo
+ *	aura // Bandeira Aura
+ *
+**/
+
+// Card form
+$gn.ready(function (checkout) {});
---
+++ modules/gateways/gofaseficartao/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Módulo Efí Cartão para WHMCS
+ * @copyright	2023 Gofas Software
+ * @see			https://gofas.net/?p=8423
+ * @license		https://gofas.net/?p=9340
+ * @support		https://gofas.net/?p=8343
+ * @version		4.0.0
+ */
+require_once __DIR__.'/includes/hooks.php';
+require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/3dsecure.php';
+require_once __DIR__.'/includes/capture.php';
